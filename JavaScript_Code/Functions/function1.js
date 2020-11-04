e16e9f1a179e21d2ac7e6ad9a8b69b7946f31a78
afsana1210 <afsanaansari1210@gmail.com>
2020-11-04 13:40:11 +0530

    functions

---
+++ JavaScript_Code/Functions/function1.js
@@ -0,0 +1,5 @@
+function myfunction(p1,p2){
+    return p1*p2;
+}
+var res=myfunction(10,10);
+console.log('result :'+res)
---
+++ JavaScript_Code/Functions/function_localvariable.js
@@ -0,0 +1,7 @@
+function LocalVariables(){
+    var carname='volvo';
+    console.log(carname)//output shown on command line
+    //document.write(carname)//ouput shown on browser
+}
+LocalVariables()
+console.log(carname) // o/p :car name is not defined ,because it is local variable,it scope is only within the function block
