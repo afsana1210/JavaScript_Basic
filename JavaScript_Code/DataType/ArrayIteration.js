4f90e36845d0cd92916866e3665310b66c8cfe71
afsana1210 <afsanaansari1210@gmail.com>
2020-11-07 12:20:23 +0530

    Array Iteration

--- JavaScript_Code/DataType/ArrayIteration.js
+++ JavaScript_Code/DataType/ArrayIteration.js
@@ -15,3 +15,32 @@ function myFunction(value, index, array) {
 };
 console.log(numbers2);
 console.log('original array :'+numbers);
+
+//filter function,The filter() method creates a new array with array elements that passes a test.
+var over18=numbers.filter(filterfunction);
+
+function filterfunction(value){
+    return value > 18;
+}
+console.log(over18);
+console.log('original array :'+numbers);
+
+//The reduce() method runs a function on each array element to produce (reduce it to) a single value.
+var red=numbers.reduce(Reducefunction);//sum=99
+
+function Reducefunction(total,value){
+    return total+value;
+}
+console.log(red);
+console.log('original array :'+numbers);
+
+//The reduce() method can accept an initial value:
+var red_init=numbers.reduce(Reducefunction,100);//sum=199
+
+function Reducefunction(total,value){
+    return total+value;
+}
+console.log(red_init);
+console.log('original array :'+numbers);
+
+
