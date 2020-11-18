a8056866b4768bccba28982c95ee299a62e2691e
afsana1210 <afsanaansari1210@gmail.com>
2020-11-18 13:20:47 +0530

    function used in  expression

---
+++ JavaScript_Code/Functions/function_as_object.js
@@ -0,0 +1,8 @@
+//JavaScript functions have both properties and methods.
+
+//The arguments.length property returns the number of arguments received when the function was invoked:
+
+function myfunction(a,b){
+    console.log(arguments.length);
+}
+myfunction(4,5);
---
+++ JavaScript_Code/Functions/function_expression.js
@@ -0,0 +1,6 @@
+//function can be used in expression
+function myFunction(a, b) {
+    return a * b;
+  }
+  var x = myFunction(4, 3) * 2;
+  console.log(x);
