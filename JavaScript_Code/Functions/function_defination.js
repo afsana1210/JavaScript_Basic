68c670e4a708ef400730ae7fbb83d08096d24a87
afsana1210 <afsanaansari1210@gmail.com>
2020-11-18 12:58:34 +0530

    function value stored into variable and called by variable

---
+++ JavaScript_Code/Functions/function_defination.js
@@ -0,0 +1,5 @@
+//After a function expression has been stored in a variable, the variable can be used as a function:
+var x=function myfunction(a,b){
+    return a+b;
+};
+console.log(x(3,4));//anonymous function
