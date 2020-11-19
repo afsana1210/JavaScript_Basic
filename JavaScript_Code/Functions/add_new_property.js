f92e24c3c1d26035d96ddc37e25b16ec91941cef
afsana1210 <afsanaansari1210@gmail.com>
2020-11-19 19:20:20 +0530

    Added new property into object

---
+++ JavaScript_Code/Functions/add_new_property.js
@@ -0,0 +1,5 @@
+//You can add new properties to existing objects.
+
+var person={fname:"john",lname:"smith",age:50};
+var new_property=person.nationality="English";
+console.log(person.fname+"'s "+"nationality is "+person.nationality+".");
